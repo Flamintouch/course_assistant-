a4b7454a931591696edfa803c55bf735a3b5dfc3
brian1454120017 <briankafulakasonde484@gmail.com>
2024-02-11 01:13:00 +0200

    assets file update - 11 - 02 - 2024.

--- success.php
+++ success.php
@@ -3,47 +3,25 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
     <link rel="stylesheet"  href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <title>Sucess</title>
+    
     <style>
-        .card-title{
+      .card-title{
             color: 	green;
-        }
-
-        .btn{
-            background-color: green;
-        }
-        
-        .main-container{
-            box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
-            margin: 30px auto;
-            padding: 1em;
-            width:40%;
-            padding: 50px 100px;
-        }
-
-
-        @media screen and (max-width: 800px) {
-                body {
-                   font-size: 12px;
-                }
-                
-              .main-container{
-                margin: 30px auto;
-                padding: 1em;
-                width:80%;
-               }
-        }
-
+          }
     </style>
 </head> 
 <body>
-<div class="main-container card text-center">
-  <div class="card-body">
-    <h1 class="card-title">Your Assignment Has Been submitted Sucessfully</h1>
-    <p class="card-text">Thank you for trusting us. Your Assignment will be Answered as soon as possible.</p>
-    <a href="main.php" class="btn btn-success">Submit More</a>
-  </div>
-</div>
+ <main class="success-screen">
+    <div class="main-container card text-center">
+      <div class="card-body">
+        <h1 class="card-title">Your Assignment Has Been submitted Sucessfully</h1>
+        <p class="card-text">Thank you for trusting us. Your Assignment will be Answered as soon as possible.</p>
+        <a href="main.php" class="btn btn-success">Submit More</a>
+      </div>
+    </div>
+ </main>
 </body>
 </html>
